dfa80734afdb9b8097e413d76e715205479f994b
Chad Elder <chud92@msn.com>
2016-08-10 20:30:14 -0400

    81016

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -10,7 +10,7 @@ app.controller('HomeController', ['$scope', function($scope) {
       and installation. Creative engineering design with some logic.  Not a bad\
       occupation.',
       muzik: 'This link will take you to some song I wrote and recorded. All\
-      instruments were played by me.  I have been listening and collecting\
+      instruments were played by me.  I have been listening to music and playing\
       instruments since I was a kid.',
       pi: 'Early in the mornings all be out there finding where my limits are.'
   };
