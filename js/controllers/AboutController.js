70b7a3bed4aa2125a64f40738d043d274fd143cf
Chad Elder <chud92@msn.com>
2016-08-14 15:30:57 -0400

    81416

--- js/controllers/AboutController.js
+++ js/controllers/AboutController.js
@@ -3,8 +3,9 @@ app.controller('AboutController', ['$scope', function($scope) {
 
   $scope.layout = {
     text1: 'In the process of working through several different platforms in \
-    the computer science field. Computer science utilizies both your analytical \
-    and artistic side, and all you need is a computer. Team oriented person \
+    the computer science field. Concentrating on design and server side development, \
+    while attending VCU Computer Science post-bac program. Computer science \
+    utilizies both your analytical and artistic side, and all you need is a computer. Team oriented person \
     driven to troubleshoot creative and analytical challenges.',
     text2: 'Born in Los Angeles. Raised in Fresno.  Moved to Maui. Met my wife. \
     Moved to San Franciso.  Moved to Richmond.  Moved back to San Franciso. \
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -6,9 +6,9 @@ app.controller('HomeController', ['$scope', function($scope) {
       art: 'I started programming in college modeling mathematical systems, which\
       is similar to OOP. Then I realized the really cool stuff was happening\
       in design.',
-      engineer: 'I started out as a sheet metal mechanic.  Design, fabrication\
-      and installation. Creative engineering design with some logic.  Not a bad\
-      occupation.',
+      engineer: 'I started out as a sheet metal mechanic.  Then went to school \
+      to get a mechanical engineering degree. Creative engineering and design with \
+      some logic.',
       muzik: 'This link will take you to some song I wrote and recorded. All\
       instruments were played by me.  I have been listening to music and playing\
       instruments since I was a kid.',
